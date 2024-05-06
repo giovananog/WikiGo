60a8ca5d3cc2259ab597160e1ef2ebc16dfc64b1
Dogolaa <lucasdogo01@gmail.com>
2024-05-06 13:59:12 -0300

    Paginacao

--- frontend_wikigo-main/frontend/src/Screens/Answers/Answers.jsx
+++ frontend_wikigo-main/frontend/src/Screens/Answers/Answers.jsx
@@ -1,27 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import DarkMode from "../../components/DarkMode/DarkMode";
 import { useLocation } from "react-router-dom";
 
 function Answers() {
   const location = useLocation();
   const { results } = location.state;
+  const [visibleCount, setVisibleCount] = useState(4); // Inicialmente mostramos 4 itens
+
+  const loadMore = () => {
+    setVisibleCount(prevCount => prevCount + 4); // Carregar mais 4 itens a cada clique
+  };
+
   return (
     <div className="Geral">
       <div className="DarkModeButton">
         <DarkMode />
       </div>
-      <div>
       <h1>Search Results</h1>
       {results.length > 0 ? (
         <ul>
-          {results.map((result, index) => (
-            <li key={index}>{result.title}</li> // Ajuste este código conforme a estrutura do seu JSON
+          {results.slice(0, visibleCount).map((result, index) => (
+            <li key={index}>
+              <h2>{result.title}</h2>
+              <a href={result.url} target="_blank" rel="noopener noreferrer">Read more</a>
+              <p>{result.abs}</p>
+            </li>
           ))}
         </ul>
       ) : (
         <p>No results found.</p>
       )}
-    </div>
+      {visibleCount < results.length && (
+        <button onClick={loadMore}>Load More Results</button>
+      )}
     </div>
   );
 }
